93ae634fd88df60bcd7a989e014101381fca8f3b
Subham Kapoor <subham.kapoor@SubhamKapoor.local>
2019-06-06 11:22:15 +0530

    Added image utils

---
+++ PythonUtils/image_utils/image_utils.py
@@ -0,0 +1,51 @@
+import operator
+import numpy as np
+from imageio import imread
+from skimage.transform import resize
+
+def load_and_align_images(filepaths, margin,image_size = 160):
+    
+    aligned_images = []
+    for filepath in filepaths:
+        img = imread(filepath)
+        aligned = resize(img, (image_size, image_size), mode='reflect')
+        aligned_images.append(aligned)
+            
+    return np.array(aligned_images)
+
+
+def prewhiten(x):
+    if x.ndim == 4:
+        axis = (1, 2, 3)
+        size = x[0].size
+    elif x.ndim == 3:
+        axis = (0, 1, 2)
+        size = x.size
+    else:
+        raise ValueError('Dimension should be 3 or 4')
+
+    mean = np.mean(x, axis=axis, keepdims=True)
+    std = np.std(x, axis=axis, keepdims=True)
+    std_adj = np.maximum(std, 1.0/np.sqrt(size))
+    y = (x - mean) / std_adj
+    return y
+
+def color_analysis(img):
+    # obtain the color palatte of the image 
+    palatte = defaultdict(int)
+    for pixel in img.getdata():
+        palatte[pixel] += 1
+    
+    # sort the colors present in the image 
+    sorted_x = sorted(palatte.items(), key=operator.itemgetter(1), reverse = True)
+    light_shade, dark_shade, shade_count, pixel_limit = 0, 0, 0, 25
+    for i, x in enumerate(sorted_x[:pixel_limit]):
+        if all(xx <= 20 for xx in x[0][:3]): ## dull : too much darkness 
+            dark_shade += x[1]
+        if all(xx >= 240 for xx in x[0][:3]): ## bright : too much whiteness 
+            light_shade += x[1]
+        shade_count += x[1]
+        
+    light_percent = round((float(light_shade)/shade_count)*100, 2)
+    dark_percent = round((float(dark_shade)/shade_count)*100, 2)
+    return light_percent, dark_percent
