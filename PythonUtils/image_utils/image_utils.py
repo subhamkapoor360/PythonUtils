36373747cb338bea703e8514eded05e84d98c007
Subham Kapoor <subham.kapoor@SubhamKapoor.local>
2019-06-10 18:52:53 +0530

    added image numpy and network utils

--- PythonUtils/image_utils/image_utils.py
+++ PythonUtils/image_utils/image_utils.py
@@ -3,7 +3,8 @@ import numpy as np
 from imageio import imread
 from skimage.transform import resize
 
-def load_and_align_images(filepaths, margin,image_size = 160):
+
+def load_and_align_images(filepaths, margin,image_size):
     
     aligned_images = []
     for filepath in filepaths:
@@ -30,22 +31,4 @@ def prewhiten(x):
     y = (x - mean) / std_adj
     return y
 
-def color_analysis(img):
-    # obtain the color palatte of the image 
-    palatte = defaultdict(int)
-    for pixel in img.getdata():
-        palatte[pixel] += 1
-    
-    # sort the colors present in the image 
-    sorted_x = sorted(palatte.items(), key=operator.itemgetter(1), reverse = True)
-    light_shade, dark_shade, shade_count, pixel_limit = 0, 0, 0, 25
-    for i, x in enumerate(sorted_x[:pixel_limit]):
-        if all(xx <= 20 for xx in x[0][:3]): ## dull : too much darkness 
-            dark_shade += x[1]
-        if all(xx >= 240 for xx in x[0][:3]): ## bright : too much whiteness 
-            light_shade += x[1]
-        shade_count += x[1]
-        
-    light_percent = round((float(light_shade)/shade_count)*100, 2)
-    dark_percent = round((float(dark_shade)/shade_count)*100, 2)
-    return light_percent, dark_percent
+
---
+++ PythonUtils/numpy_utils/numpy_utils.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+def l2_normalize(x, axis=-1, epsilon=1e-10):
+    output = x / np.sqrt(np.maximum(np.sum(np.square(x), axis=axis, keepdims=True), epsilon))
+    return output
+
