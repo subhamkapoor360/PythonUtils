fc646dd9217ff18a1b46cfc1037a463531649ce0
Subham Kapoor <subham.kapoor@SubhamKapoor.local>
2019-06-23 11:42:41 +0530

    added requiremnts.txt

---
+++ PythonUtils/pandas_utils/technical_indicators.py
@@ -0,0 +1,19 @@
+import numpy  
+import pandas as pd  
+import math as m
+
+
+
+#Momentum  
+def MOM(df, n):  
+    M = pd.Series(df['Close'].diff(n), name = 'Momentum_' + str(n))  
+    df = df.join(M)  
+    return df
+
+#Rate of Change  
+def ROC(df, n):  
+    M = df['Close'].diff(n - 1)  
+    N = df['Close'].shift(n - 1)  
+    ROC = pd.Series(M / N, name = 'ROC_' + str(n))  
+    df = df.join(ROC)  
+    return df
