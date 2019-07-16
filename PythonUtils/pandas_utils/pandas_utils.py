b7499e9b2c6c73db68e23dc5f0505716845fd971
Subham Kapoor <subham.kapoor@SubhamKapoor.local>
2019-07-16 18:24:38 +0530

    added memory reduce pandas dataframe

--- PythonUtils/pandas_utils/pandas_utils.py
+++ PythonUtils/pandas_utils/pandas_utils.py
@@ -27,3 +27,29 @@ def get_feature_unique(df):
 	nunique_df = pd.DataFrame(dict(nunique_map),index = [0]).T
 	nunique_df.columns = ['Unique Feature Count']
 	return nunique_df
+
+def memory_reduce(df):
+    for col in df.columns:
+        col_type = df[col].dtype
+        if(col_type != object):
+            min_val = min(df[col])
+            max_val = max(df[col])
+            if(str(col_type)[:3] == 'int'):
+                if(min_val > np.iinfo(np.int8).min and max_val < np.iinfo(np.int8).max):
+                    df[col] = df[col].astype(np.int8)
+                elif(min_val > np.iinfo(np.int16).min and max_val < np.iinfo(np.int16).max):
+                    df[col] = df[col].astype(np.int16)
+                elif(min_val > np.iinfo(np.int32).min and max_val < np.iinfo(np.int32).max):
+                    df[col] = df[col].astype(np.int32)
+                elif(min_val > np.iinfo(np.int64).min and max_val < np.iinfo(np.int64).max):
+                    df[col] = df[col].astype(np.int64)
+            else:
+                if(min_val > np.finfo(np.float16).min and max_val < np.finfo(np.float16).max):
+                    df[col] = df[col].astype(np.float16)
+                elif(min_val > np.finfo(np.float32).min and max_val < np.finfo(np.float32).max):
+                    df[col] = df[col].astype(np.float32)
+                else:
+                    df[col] = df[col].astype(np.float64)
+
+    return df
+    
